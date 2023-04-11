aacba312f8b639d1d6b8ccd61d9d6ecf37f0724b
Elise Flaneuse <eliseflaneuse@gmail.com>
2023-04-11 17:39:19 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { marked } from 'marked';
+
+//: a heading element (H1 size), 
+//a sub heading element (H2 size), a link, inline code, a code block, 
+//a list item, a blockquote, an image, and bolded text.
 
 function App() {
+  const [text, setText] = useState(`
+  # This is a H1 heading
+  ## This is a H2 heading
+  ### This is a H3 heading
+
+  [I'm an inline-style link](https://www.google.com)
+
+  Inline \`code\` has \`back-ticks around\` it.
+
+  \`\`\`
+  const freeCodeCamp = "Coding is awesome!";
+
+  function App() {
+    render(){
+      return <h1>{freeCodeCamp}</h1>
+    }
+  }
+  \`\`\`
+
+  * Unordered list can use asterisks
+  - Or minuses
+  + Or pluses
+
+  > Blockquotes are very handy in email to emulate reply text.
+  > This line is part of the same quote.
+
+  Emphasis, aka italics, with *asterisks* or _underscores_.
+
+  Strong emphasis, aka bold, with **asterisks** or __underscores__.
+
+  Combined emphasis with **asterisks and _underscores_**.
+
+  ![freeCodeCamp Logo](https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/FreeCodeCamp_logo.svg/320px-FreeCodeCamp_logo.svg.png  )
+
+  `)
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <header className='header'>
+      <h1>Markdown Previewer</h1>
       </header>
+    
+
+      <div className="App-wraper">
+        
+        <div className="editorWrap">
+          <textarea 
+            id="editor" 
+            onChange={(event) => {setText(event.target.value)}}
+            value={text} 
+            spellCheck="false"
+            ></textarea>
+        </div>
+
+        <div className="previewerWrap">
+          <div id="preview"
+          dangerouslySetInnerHTML={{__html: marked(text, { breaks: true })}}
+          ></div>
+        </div>
+        
+      </div>
+
+      <footer className='footer'>
+        <span>
+          freeCodeCamp Front End Development Libraries Projects - Build a Markdown Previewer / 
+          <a href="https://github.com/eliseflaneuse" target="_blank" rel="noreferrer"> Elise Flaneuse </a>
+          / 2023
+        </span>
+      </footer>
     </div>
   );
 }
